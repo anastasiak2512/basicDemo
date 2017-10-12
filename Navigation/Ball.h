3a2634c3304bdb2366568e8cc1809c6c6aa9f224
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2017-10-12 03:19:10 +0300

    Navigation samples added

---
+++ Navigation/Ball.h
@@ -0,0 +1,20 @@
+//
+// Navigate to class, file, symbol
+//
+
+class Ball {
+private:
+    int diameter;
+};
+
+class colorBall : public Ball {
+private:
+    int color;
+public:
+    void colorBallCall() { }
+};
+
+class metaBall {
+private:
+    int diameter;
+};
---
+++ Navigation/SomeSources.cpp
@@ -0,0 +1,13 @@
+// 1. Imports Hierarchy (Shift + Alt + H) on this file
+// 2. Call Hierarchy (Ctrl + Alt + H) on colorBallCall
+// 3. Type Hierarchy (Ctrl + H) on colorBall
+
+#include "Ball.h"
+#include <vector>
+
+void doSmth() {
+    std::vector<colorBall> v;
+    for (auto &&item : v) {
+        item.colorBallCall();
+    }
+}
