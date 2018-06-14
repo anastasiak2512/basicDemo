5d7cc65ef8e9298338778c8c58a3d8dfd82ae648
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2018-06-14 20:06:43 +0300

    updated generation part

--- Code_Generation/Distance.h
+++ Code_Generation/Distance.h
@@ -5,29 +5,13 @@
 #define DEMOMORE_DISTANCE_H
 
 #include <vector>
+#include <ostream>
 
 class Distance {
 private:
     int feet = 0;
     int inches = 0;
 public:
-    Distance(int feet , int inches) : feet(feet) , inches(inches)
-    {}
-
-    Distance operator-()
-    {
-        feet = -feet;
-        inches = -inches;
-        return {feet, inches};
-    }
-
-    bool operator<(const Distance &d) const
-    {
-        if (feet < d.feet) {
-            return true;
-        }
-        return feet == d.feet && inches < d.inches;
-    }
 
     void some_cycle() {
         std::vector<int> var{1,2,3,4,5};
