9bd0a85a4e2a76e876c543a293487a05876e5231
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2018-10-10 15:18:12 +0300

    Added multiline TODO to the demo (from v18.3)

---
+++ Documentation/Task_reminders.cpp
@@ -0,0 +1,16 @@
+#include <ctime>
+#include <iostream>
+
+// TODO: rework function
+// calculate local time
+// return string not int
+// use chrono::system_clock
+int calculate_date() {
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+
+    return (now->tm_year + 1900) +
+           (now->tm_mon + 1) +
+           (now->tm_mday);
+}
+
