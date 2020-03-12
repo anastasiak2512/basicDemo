86c5ca8f4ecffdc98bb2c4964a00213927dd8855
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2020-03-12 13:06:47 +0300

    Updated Change Signature example, add Extract Parameter with the initializer value

--- Refactoring/ChangeSignature.cpp
+++ Refactoring/ChangeSignature.cpp
@@ -1,6 +1,8 @@
 //Use Change Signature (Ctrl+F6 on Windows/Linux, Cmd+F6 on macOS) to:
 //- Add another Person Data parameter to the callPerson function.
 //- Swap two input parameters in the callPerson function.
+//- Use Change Signature to add rate_cf with the default value, use completion for DEFAULT_RATE
+//- Extract parameter (extra_cf), check the value from the initializer is used in the usages
 
 #define DEFAULT_RATE 100
 
@@ -10,13 +12,14 @@ typedef struct PersonData {
     const char *name;
 } PersonData;
 
-void callPerson(PersonData *p) {
-    p->rate_idx = p->int_idx * 100;
+void callPerson(PersonData *p, int rate_cf) {
+    int extra_cf = 200;
+    p->rate_idx = p->int_idx * 100 * extra_cf;
 
     //TODO: implement actual call
 }
 
 void usage() {
     PersonData p = {1, 100, "Bjarne Stroustrup"};
-    callPerson(&p);
+    callPerson(&p, DEFAULT_RATE);
 }
