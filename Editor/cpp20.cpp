a3d2d528482bf505a56edf47ee30a11ca9212faf
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2020-07-20 13:11:53 +0300

    Updated sample for C++20 for loop with initializer

--- Editor/cpp20.cpp
+++ Editor/cpp20.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <vector>
 
+// Explicit(bool)
+#define LEVEL_NUM 100
+
+constexpr bool abc(int num) {
+    return (num > LEVEL_NUM);
+}
+
+class Foo {
+public:
+    explicit(abc(1)) Foo(){ abc(1);}
+};
+
+// Range-based for loop with initializer
+void test() {
+    std::vector<int> vec = {1, 3, 5, 7, 9};
+
+    for (auto sz = vec.size(); auto item : vec) {
+        std::cout << --sz + item << ' ';
+    }
+}
+
+// Designated initializer
 struct Person
 {
     std::string name{};
@@ -12,25 +35,11 @@ struct Employee : public Person
     unsigned salary{10000};
 };
 
-#define LEVEL_NUM 100
-
-constexpr bool abc(int num) {
-    return (num > LEVEL_NUM);
-}
-
-class Foo {
-public:
-    explicit(abc(1)) Foo(){ abc(1);}
-};
-
-void printThisInteger(int) {}
-
 int main() {
     Employee emp { {.name="John", .surname="Wick", .age=40}, 5000};
 
     std::cout << emp.name << " " << emp.surname << ": age "
               << emp.age << ", " << emp.salary << "\n";
 
-    printThisInteger(10);
     return 0;
 }
