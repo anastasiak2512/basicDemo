84198bba135cf29dafb92a29bf7a09228db863c6
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2020-11-24 23:29:10 +0300

    Updated MISRA sample

--- Code_Analysis/CodeAnalysis.cpp
+++ Code_Analysis/CodeAnalysis.cpp
@@ -119,6 +119,17 @@ void test2() {
 //========================================================
 // MISRA checks: https://confluence.jetbrains.com/display/CLION/MISRA+checks+supported+in+CLion
 
+template<typename T> void f(T);
+template<> void f<uint16_t>(uint16_t);
+template <> void f<int16_t>(int16_t);
+void b() {
+    uint16_t u16a = 0U;
+    f(u16a);
+    u16a = u16a + 0x8000;
+    f(u16a);
+}
+
+
 #define SHIFT_8 8
 
 unsigned char func(unsigned char num) {
@@ -126,6 +137,13 @@ unsigned char func(unsigned char num) {
 }
 
 void discarded(unsigned char counter) {
+    int32_t code[5];
+    code[0] = 109;
+    code[1] = 100;
+    code[2] = 052;
+    code[3] = 071;
+    code[4] = '\100';
+
     unsigned char foo = 1;
     foo <<= SHIFT_8;
 
