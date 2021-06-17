3c3652d250037d01327aea06066c8a69644a49e1
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2021-06-17 18:42:56 +0300

    Natvis sample improved

--- Debug_extra/Win_debug_natvis.cpp
+++ Debug_extra/Win_debug_natvis.cpp
@@ -8,10 +8,22 @@ class MyPoint {
 public:
     MyPoint(int x, int y) : x(x), y(y) {}
 };
+enum class Color {
+    Black, White, Yellow, Red, Blue, Green
+};
+
+class MyColoredPoint : public MyPoint {
+    Color cl{Color::Black};
+public:
+    MyColoredPoint(Color c, int x, int y) : MyPoint(x, y), cl(c) {}
+    explicit MyColoredPoint(MyPoint p): MyPoint(p) {}
+};
 
 int main() {
     MyPoint mypoint_{1,2};
     MyPoint mypoint_zero{0,0};
+    MyColoredPoint cp_zero(mypoint_zero);
+    MyColoredPoint cp_(Color::Blue, 4, 5);
 
     std::vector<MyPoint> vector_of_points = {{1,1}};
 
