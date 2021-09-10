55a062e8a8e41d2715929443c1ab4820660331a0
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2021-09-10 15:31:07 +0300

    MISRA checks in a separate file

--- Code_Analysis/MISRA_inspections.cpp
+++ Code_Analysis/MISRA_inspections.cpp
@@ -1,19 +1,55 @@
 // To enable MISRA Check
 // go to Settings/Preferences | Editor | Inspections | C/C++ | General | MISRA Checks
 // note that clang-tidy is built one and calngd via clang-tidy is enabled
+// MISRA checks: https://confluence.jetbrains.com/display/CLION/MISRA+checks+supported+in+CLion
 
-
-void check(int argc[]) { // 3-1-3
-    switch (argc[0]) {
-        default: // 6-4-6
-        case 1:
-            break;
-    }
-
-    bool a = true, b = false, c = a < b; // 4-5-1
-
-    int ***pInt = nullptr; // 5-0-19
-
-    float f = (float) 1; // 5-2-4
+template<typename T> void f(T);
+template<> void f<uint16_t>(uint16_t);
+template <> void f<int16_t>(int16_t);
+void b() {
+    uint16_t u16a = 0U;
+    f(u16a);
+    u16a = u16a + 0x8000;
+    f(u16a);
 }
+
+
+#define SHIFT_8 8
+
+unsigned char func(unsigned char num) {
+    return num;
+}
+
+void discarded(unsigned char counter) {
+    int32_t code[5];
+    code[0] = 109;
+    code[1] = 100;
+    code[2] = 052;
+    code[3] = 071;
+    code[4] = '\100';
+
+    unsigned char foo = 1;
+    foo <<= SHIFT_8;
+
+    func(counter * foo);
+}
+
+struct S {
+    signed int a : 1;
+    signed int : 1;
+    signed int : 0;
+    signed int b : 2;
+    signed int : 2;
+};
+
+void process_ptr(int32_t *int_ptr) {}
+
+void operate() {
+    process_ptr(0);
+}
+
+class OpClass {
+public:
+    OpClass *operator&() {};
+};
 
