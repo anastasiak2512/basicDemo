9e1eb51a89532676e6608668f478dcbaf1af5f03
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2021-09-23 12:50:21 +0300

    Added new hints

--- Documentation/TypeDebug.cpp
+++ Documentation/TypeDebug.cpp
@@ -1,5 +1,6 @@
 //Call Quick Doc (Ctrl+Q on Windows/Linux, F1 on macOS) on op variable to see its type.
 //Change the call arguments, check that the type is updated.
+#include <iostream>
 
 template<typename T, typename U>
 auto doOperation(T t, U u) -> decltype(t + u) {
@@ -10,4 +11,6 @@ void fun_type() {
     auto op = doOperation(3, 0);
     auto op1 = doOperation(3L, 0);
     auto op2 = doOperation(3.0, 0);
+
+    std::cout << op << " " << op1 << " " << op2;
 }
---
+++ Editor/hints.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <functional>
+
+// More for lambdas in TypeDebug.cpp
+// And in ExtractLambda.cpp
+
+std::function<int(int)> F(int count)
+{
+    return [](int m) { return 0; };
+}
+
+//=====================
+
+class Foo {
+
+};
+template<typename T>
+class Bar {};
+void func(const Bar<Foo>& c) {
+    auto f = c;
+}
+//=====================
+
+int main() {
+    int a = 1, b = 2;
+    const auto&[x, y] = std::tie(a, b);
+    auto[z, w] = std::tie(a, b);
+    const auto tuple = std::make_tuple(1, 'a', 2.3);
+    const auto& [i, c, d] = tuple;
+}
+
